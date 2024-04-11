209e84a2fe5da0229553726e8a5616e2c8590190
Kristóf Szőllősi <szkristof2h@gmail.com>
2024-04-11 21:08:11 +0200

    add very basic api key protection

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI
-from openai import OpenAI
+from fastapi import FastAPI, Security, HTTPException, Depends
+from fastapi.security.api_key import APIKeyHeader, APIKey
+from openai import OpenAI, OpenAIError
 from pydantic import BaseModel
+from starlette.status import HTTP_401_UNAUTHORIZED
+import os
 
 client = OpenAI()
 
 app = FastAPI()
 
-class Echo(BaseModel):
-    text: str
+API_KEY = os.environ['ECHO_API_KEY']
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+
+
+def authenticate(api_key: str = Security(api_key_header)):
+  if (API_KEY != api_key):
+    raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="Missing or invalid API key")
 
 def generate(text):
   try:
@@ -23,11 +31,14 @@ def generate(text):
     print(f"Error: {e}")
     return "No suggestions"
 
+class Echo(BaseModel):
+  text: str
+
 @app.get("/")
-async def root():
+async def root(api_key: APIKey = Depends(authenticate)):
   return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
 @app.post("/suggestion")
-def create_suggestion(echo_text: Echo):
-  suggestion = generate(echo_text.text)
+def create_suggestion(echo: Echo, api_key: APIKey = Depends(authenticate)):
+  suggestion = generate(echo.text)
   return {"suggestion": suggestion}
